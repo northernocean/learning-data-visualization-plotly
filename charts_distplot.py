77b70961a9536dbdd296f47cd1c6488547894dd9
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-17 16:03:28 -0400

    distplot charts

---
+++ charts_distplot.py
@@ -0,0 +1,11 @@
+import plotly.figure_factory as ff
+import numpy as np
+
+hist_data = [
+    (np.random.randn(200) - 2), 
+    (np.random.randn(200)),
+    (np.random.randn(200) + 2)
+]
+group_labels = ["X1", "X2", "X3"]
+fig = ff.create_distplot(hist_data=hist_data, group_labels=group_labels)
+fig.show()
