6f55101abf63bdf36c30da90f4f5ca6121d83871
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-18 14:05:43 -0400

    creating a dashboard with plotly graphs

--- app.py
+++ app.py
@@ -1,55 +1,72 @@
 import dash
 import dash_core_components as core
 import dash_html_components as html
+import plotly.graph_objs as go
+import numpy as np
 
 app = dash.Dash()
 
-colors = {
-    "background": "#111111",
-    "text": "#7fdbff"
-}
+np.random.seed(42)
+
+xs = []
+ys = []
+for _ in range(0, 2):
+    xs.append(np.random.randint(1, 101, 100))
+    ys.append(np.random.randint(1, 101, 100))
 
 app.layout = html.Div(
-    children=[
-        html.H1(
-            "Hello Dash",
-            style={
-                "textAlign": "center",
-                "color": colors["text"]
-            }
-        ),
-        html.Div("Dash: Web Dashboards with Python"),
+    [
+        # First Graph Object
         core.Graph(
-            id="example",
+            id="scatterplot1",
             figure={
                 "data": [
-                    {
-                        "x": [1, 2, 3],
-                        "y": [4, 1, 2],
-                        "type": "bar",
-                        "name": "SF"
-                    },
-                    {
-                        "x": [1, 2, 3],
-                        "y": [2, 4, 5],
-                        "type": "bar",
-                        "name": "NYC"
-                    }
+                    go.Scatter(
+                        x=xs[0],
+                        y=ys[0],
+                        mode="markers",
+                        marker=dict(
+                            size=12,
+                            color="rgb(51, 204,153)",
+                            symbol="pentagon",
+                            line={"width": 2}
+                        )
+                    )
                 ],
-                "layout": {
-                    "title": "Bar Plots",
-                    "plot_bgcolor": colors["background"],
-                    "paper_bgcolor": colors["background"],
-                    "font": {
-                        "color": colors["text"]
-                    }
-                }
+                "layout": go.Layout(
+                    title="Scatterplot",
+                    xaxis=dict(
+                        title="X axis"
+                    )
+                )
+            }
+        ),
+        # second graph object
+        core.Graph(
+            id="scatterplot2",
+            figure={
+                "data": [
+                    go.Scatter(
+                        x=xs[1],
+                        y=ys[1],
+                        mode="markers",
+                        marker=dict(
+                            size=12,
+                            color="rgb(200, 204,53)",
+                            symbol="pentagon",
+                            line={"width": 2}
+                        )
+                    )
+                ],
+                "layout": go.Layout(
+                    title="Scatterplot",
+                    xaxis=dict(
+                        title="X axis"
+                    )
+                )
             }
         )
-    ],
-    style={
-        "backgroundColor": colors["background"]
-    }
+    ]
 )
 
 if __name__ == "__main__":
