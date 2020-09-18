0e762a02ba13a38888287ce170a9ff49364c14e8
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-18 09:48:34 -0400

    Hello World Dash

---
+++ app.py
@@ -0,0 +1,37 @@
+import dash
+import dash_core_components as core
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    children=[
+        html.H1("Hello Dash"),
+        html.Div("Dash: Web Dashboards with Python"),
+        core.Graph(
+            id="example",
+            figure={
+                "data": [
+                    {
+                        "x": [1, 2, 3],
+                        "y": [4, 1, 2],
+                        "type": "bar",
+                        "name": "SF"
+                    },
+                    {
+                        "x": [1, 2, 3],
+                        "y": [6, 4, 5],
+                        "type": "bar",
+                        "name": "NYC"
+                    }
+                ],
+                "layout": {
+                    "title": "Bar Plots"
+                }
+            }
+        )
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server()
