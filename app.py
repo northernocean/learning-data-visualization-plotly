7ea71da68f557b8ea8e504b3ed7061d84e1f6f69
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-19 10:21:20 -0400

    markdown example

--- app.py
+++ app.py
@@ -1,54 +1,26 @@
 import dash
 import dash_core_components as core
 import dash_html_components as html
-import plotly.graph_objs as go
-import numpy as np
 
 app = dash.Dash()
 
-app.layout = html.Div(
-    [
-        html.Div(
-            [
-                html.Label("Dropdown"),
-                core.Dropdown(
-                    options=[
-                        {"label": "New York City", "value": "NYC"},
-                        {"label": "San Francisco", "value": "SF"}
-                    ],
-                    value="SF"
-                )
-            ],
-            style={"margin": "40px"}
-        ),
-        html.Div(
-            [
-                html.Label("Slider"),
-                core.Slider(
-                    min=-10,
-                    max=10,
-                    step=1,
-                    value=0,
-                    marks={i: i for i in range(-10, 10)}
-                )
-            ],
-            style={"margin": "40px"}
-        ),
-        html.Div(
-            [
-                html.Label("Radio"),
-                core.RadioItems(
-                    options=[
-                        {"label": "New York City", "value": "NYC"},
-                        {"label": "San Francisco", "value": "SF"},
-                    ],
-                    value="SF"
-                )
-            ],
-            style={"margin": "40px"}
-        )
-    ]
-)
+markdown_text = '''
+### Dash and Markdown
 
-if __name__ == "__main__":
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/) specification of Markdown.
+
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+
+Markdown includes syntax for things like **bold text** and *italics*,
+[links](http://commonmark.org/help), inline `code` snippets, lists,
+quotes, and more.
+'''
+
+app.layout = html.Div([
+    core.Markdown(children=markdown_text)
+])
+
+if __name__ == '__main__':
     app.run_server()
