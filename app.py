8dbaf0bd3f329a684508502490fbda65dce3b8c1
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-20 17:02:50 -0400

    callbacks with state

--- app.py
+++ app.py
@@ -1,94 +1,33 @@
-import os
-import base64
+######
 import dash
-import dash_core_components as core
+import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import pandas as pd
-
-df = pd.read_csv("data/wheels.csv")
-print(df)
+from dash.dependencies import Input, Output, State
 
 app = dash.Dash()
 
-
-def encode_image(file_path):
-    encoded = base64.b64encode(open(file_path, "rb").read())
-    return f"data:image/png;base64,{encoded.decode()}"
-
-
-app.layout = html.Div(
-    [
-        # Radio Button
-        html.Div(
-            className="dash-app",
-            children=[
-                core.RadioItems(
-                    id="wheels",
-                    options=[
-                        {"label": i, "value": i} for i in df["wheels"].unique()
-                    ],
-                    value=1
-                )
-            ]
-        ),
-        # output for number of wheels
-        html.Div(
-            className="dash-app",
-            id="wheels-output",
-        ),
-        # Color Dropdown
-        html.Div(
-            className="dash-app",
-            children=[
-                core.RadioItems(
-                    id="colors",
-                    options=[
-                        {"label": i, "value": i} for i in df["color"].unique()
-                    ],
-                    value="blue"
-                )
-
-            ]
-        ),
-        # output for number of colors
-        html.Div(
-            className="dash-app",
-            id="colors-output",
-        ),
-        html.Img(
-            className="dash-app",
-            id="display-image",
-            src="children",
-            height=300
-
-        )
-    ]
-)
+app.layout = html.Div([
+    dcc.Input(
+        id='number-in',
+        value=1,
+        style={'fontSize': 28}
+    ),
+    html.Button(
+        id='submit-button',
+        n_clicks=0,
+        children='Submit',
+        style={'fontSize': 28}
+    ),
+    html.H1(id='number-out')
+])
 
 
 @app.callback(
-    Output("wheels-output", "children"),
-    [Input("wheels", "value")])
-def callback_wheels(wheels_value):
-    return wheels_value
-
-
-@app.callback(
-    Output("colors-output", "children"),
-    [Input("colors", "value")])
-def callback_colors(colors_value):
-    return colors_value
-
-
-@app.callback(
-    Output("display-image", "src"),
-    [Input("wheels", "value"), Input("colors", "value")]
-)
-def callback_image(wheel, color):
-    path = df[(df["wheels"] == wheel) & (df["color"] == color)].iloc[0]["image"]
-    path = os.path.join("data", "images", path)
-    return encode_image(path)
+    Output('number-out', 'children'),
+    [Input('submit-button', 'n_clicks')],   # input triggers output - binds to param1 (n_clicks)
+    [State('number-in', 'value')])          # state stores data - binds to param2 (number)
+def output(n_clicks, number):
+    return f"{number} displayed after {n_clicks} clicks"
 
 
 if __name__ == '__main__':
