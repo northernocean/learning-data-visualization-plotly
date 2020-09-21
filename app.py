2fb02204235d3d0f2e2e7d6ff74b8cf551fa4e88
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-21 10:38:33 -0400

    updating graphs

--- app.py
+++ app.py
@@ -1,89 +1,111 @@
 #######
-# Here we'll make a scatter plot with fake data that is
-# intentionally denser on the left, with overlapping data points.
-# We'll use Selection Data to uncover the difference.
+# This shows the mpg.csv dataset as a spread out scatter plot
+# that sends hoverData to another graph via callback, and to
+# a Markdown component through a second callback.
 ######
 import dash
-import dash_core_components as core
+import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
-import numpy as np
 import pandas as pd
+from numpy import random
 
 app = dash.Dash()
 
-# create x and y arrays
-np.random.seed(10)
-x1 = np.linspace(0.1, 5, 50)        # left half
-x2 = np.linspace(5.1, 10, 50)       # right half
-y = np.random.randint(0, 50, 50)    # 50 random points
+df = pd.read_csv('data/mpg.csv')
 
-# create three "half DataFrames"
-df1 = pd.DataFrame({'x': x1, 'y': y})
-df2 = pd.DataFrame({'x': x1, 'y': y})
-df3 = pd.DataFrame({'x': x2, 'y': y})
+# Add a random "jitter" to model_year to spread out the plot
+df['year'] = df['model_year'] + random.randint(-4, 5, len(df)) * 0.10
 
-# combine them into one DataFrame (df1 and df2 points overlap!)
-df = pd.concat([df1, df2, df3])
-
-app.layout = html.Div(
-    [
-        html.Div(
-            [
-                core.Graph(
-                    id='plot',
-                    figure={
-                        'data': [
-                            go.Scatter(
-                                x=df['x'],
-                                y=df['y'],
-                                mode='markers'
-                            )
-                        ],
-                        'layout': go.Layout(
-                            title='Random Scatterplot',
-                            hovermode='closest'
-                        )
-                    }
+app.layout = html.Div([
+    html.Div([   # this Div contains our scatter plot
+        dcc.Graph(
+            id='mpg_scatter',
+            figure={
+                'data': [go.Scatter(
+                    x=df['year'] + 1900,  # our "jittered" data
+                    y=df['mpg'],
+                    text=df['name'],
+                    hoverinfo='text',
+                    mode='markers'
+                )],
+                'layout': go.Layout(
+                    title='mpg.csv dataset',
+                    xaxis={'title': 'model year'},
+                    yaxis={'title': 'miles per gallon'},
+                    hovermode='closest'
                 )
-            ],
-            style={'width': '30%', 'display': 'inline-block'}
-        ),
-        html.Div(
-            [
-                html.H1(
-                    id='density',
-                    style={'paddingTop': 25}
-                )
-            ],
-            style={
-                'width': '30%',
-                'display': 'inline-block',
-                'verticalAlign': 'top'
             }
+        )], style={'width': '40%', 'display': 'inline-block'}),
+    html.Div([  # this Div contains our output graph and vehicle stats
+        dcc.Graph(
+            id='mpg_line',
+            figure={
+                'data': [go.Scatter(
+                    x=[0, 1],
+                    y=[0, 1],
+                    mode='lines'
+                )],
+                'layout': go.Layout(
+                    title='acceleration',
+                    margin={'l': 0}
+                )
+            }
+        ),
+        dcc.Markdown(
+            id='mpg_stats'
         )
-    ]
-)
+    ], style={'width': '30%', 'height': '50%', 'display': 'inline-block'})
+])
 
 
 @app.callback(
-    Output('density', 'children'),
-    [Input('plot', 'selectedData')]
+    Output('mpg_line', 'figure'), [Input('mpg_scatter', 'hoverData')]
 )
-def find_density(selectedData):
-    pts = len(selectedData['points'])
-    rng_or_lp = list(selectedData.keys())  # boxed range or lassoed points
-    rng_or_lp.remove('points')
-    max_x = max(selectedData[rng_or_lp[0]]['x'])
-    min_x = min(selectedData[rng_or_lp[0]]['x'])
-    max_y = max(selectedData[rng_or_lp[0]]['y'])
-    min_y = min(selectedData[rng_or_lp[0]]['y'])
-    area = (max_x - min_x) * (max_y - min_y)
-    d = pts / area
-    return 'Density = {:.2f}'.format(d)
+def callback_graph(hoverData):
+    v_index = hoverData['points'][0]['pointIndex']
+    fig = {
+        'data': [
+            go.Scatter(
+                x=[0, 1],
+                y=[0, 60 / df.iloc[v_index]['acceleration']],
+                mode='lines',
+                line={'width': 2 * df.iloc[v_index]['cylinders']}
+            )
+        ],
+        'layout':
+        go.Layout(
+            title=df.iloc[v_index]['name'],
+            xaxis={'visible': False},
+            yaxis={
+                'visible': False,
+                'range': [0, 60 / df['acceleration'].min()]
+            },
+            margin={'l': 0},
+            height=300
+        )
+    }
+    return fig
+
+
+@app.callback(
+    Output('mpg_stats', 'children'),
+    [Input('mpg_scatter', 'hoverData')]
+)
+def callback_stats(hoverData):
+    v_index = hoverData['points'][0]['pointIndex']
+    stats = """
+        {} cylinders  
+        {}cc displacement  
+        0 to 60mph in {} seconds
+        """.format(
+        df.iloc[v_index]['cylinders'],
+        df.iloc[v_index]['displacement'],
+        df.iloc[v_index]['acceleration']
+    )
+    return stats
 
 
 if __name__ == '__main__':
     app.run_server()
-
