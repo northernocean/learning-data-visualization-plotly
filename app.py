46f040b45986f1a6fa042bd232da2e52ebaca6b6
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-21 15:33:22 -0400

    milestone project page layout

--- app.py
+++ app.py
@@ -3,108 +3,119 @@
 # that sends hoverData to another graph via callback, and to
 # a Markdown component through a second callback.
 ######
+import os
+import datetime
 import dash
-import dash_core_components as dcc
+import dash_core_components as core
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
+import pandas_datareader as data
 from numpy import random
+
+try:
+    df = pd.read_csv('data/stocks.csv', index_col=None)
+except FileNotFoundError:
+    df = None
+
+if df is None:
+    print('Retrieving data from yahoo api...')
+    tickers = ['WORK', 'HA', 'NGVC']
+    start_date = pd.to_datetime('2020-01-01')
+    end_date = pd.to_datetime('today')
+
+    df = []
+    for ticker in tickers:
+        temp = data.DataReader(ticker, 'yahoo', start_date, end_date)
+        if temp is not None:
+            temp.insert(0, 'Ticker', ticker)
+            temp.reset_index(level=0, inplace=True)
+            df.append(temp)
+    if df:
+        df = pd.concat(df, ignore_index=True)
+        df.to_csv('data/stocks.csv', index=False)
 
 app = dash.Dash()
 
-df = pd.read_csv('data/mpg.csv')
-
-# Add a random "jitter" to model_year to spread out the plot
-df['year'] = df['model_year'] + random.randint(-4, 5, len(df)) * 0.10
-
-app.layout = html.Div([
-    html.Div([   # this Div contains our scatter plot
-        dcc.Graph(
-            id='mpg_scatter',
-            figure={
-                'data': [go.Scatter(
-                    x=df['year'] + 1900,  # our "jittered" data
-                    y=df['mpg'],
-                    text=df['name'],
-                    hoverinfo='text',
-                    mode='markers'
-                )],
-                'layout': go.Layout(
-                    title='mpg.csv dataset',
-                    xaxis={'title': 'model year'},
-                    yaxis={'title': 'miles per gallon'},
-                    hovermode='closest'
+app.layout = html.Div(
+    className="container",
+    children=[
+        html.Div(
+            className="4d7000",
+            children=[
+                html.Div(
+                    className="2e6000",
+                    children=[
+                        html.Div(
+                            className="2e6003",
+                            id="ddl_stocks",
+                            children=[
+                                "Select Stock Ticker: ",
+                                core.Dropdown()
+                            ]
+                        ),
+                        html.Div(
+                            className="2e6006",
+                            id="dtp_dates",
+                            children=[
+                                "Select Start and End Dates: ",
+                                core.DatePickerRange()
+                            ]
+                        ),
+                        html.Div(
+                            className="2e6009",
+                            id="btnSubmit",
+                            children=[
+                                html.Button("Submit")
+                            ]
+                        )
+                    ]
+                ),
+                html.Div(
+                    className="42c090",
+                    children=[
+                        core.Graph(
+                            id="stocks_graph",
+                            figure={
+                                "data": [
+                                    {
+                                        "x": [1, 2, 3],
+                                        "y": [5, 7, 6]
+                                    }
+                                ],
+                                "layout": {
+                                    "title": "Stocks Graph"
+                                }
+                            }
+                        )
+                    ]
                 )
-            }
-        )], style={'width': '40%', 'display': 'inline-block'}),
-    html.Div([  # this Div contains our output graph and vehicle stats
-        dcc.Graph(
-            id='mpg_line',
-            figure={
-                'data': [go.Scatter(
-                    x=[0, 1],
-                    y=[0, 1],
-                    mode='lines'
-                )],
-                'layout': go.Layout(
-                    title='acceleration',
-                    margin={'l': 0}
-                )
-            }
-        ),
-        dcc.Markdown(
-            id='mpg_stats'
+            ]
         )
-    ], style={'width': '30%', 'height': '50%', 'display': 'inline-block'})
-])
+    ]
+)
 
 
 @app.callback(
-    Output('mpg_line', 'figure'), [Input('mpg_scatter', 'hoverData')]
+    Output("stocks_graph", "figure"),
+    [Input("ddl_stocks", "value")]
 )
-def callback_graph(hoverData):
-    v_index = hoverData['points'][0]['pointIndex']
-    fig = {
-        'data': [
-            go.Scatter(
-                x=[0, 1],
-                y=[0, 60 / df.iloc[v_index]['acceleration']],
-                mode='lines',
-                line={'width': 2 * df.iloc[v_index]['cylinders']}
-            )
+def update_title(arg):
+    if arg is None:
+        arg = "Foo"
+    figure = {
+        "data": [
+            {
+                "x": [1, 2, 3],
+                "y": [5, 7, 8]
+            }
         ],
-        'layout':
-        go.Layout(
-            title=df.iloc[v_index]['name'],
-            xaxis={'visible': False},
-            yaxis={
-                'visible': False,
-                'range': [0, 60 / df['acceleration'].min()]
-            },
-            margin={'l': 0},
-            height=300
-        )
+        "layout": {
+            "title": arg
+        }
     }
-    return fig
-
-
-@app.callback(
-    Output('mpg_stats', 'children'),
-    [Input('mpg_scatter', 'hoverData')]
-)
-def callback_stats(hoverData):
-    v_index = hoverData['points'][0]['pointIndex']
-    stats = """
-        {} cylinders  
-        {}cc displacement  
-        0 to 60mph in {} seconds
-        """.format(
-        df.iloc[v_index]['cylinders'],
-        df.iloc[v_index]['displacement'],
-        df.iloc[v_index]['acceleration']
-    )
-    return stats
+    return figure
 
 
 if __name__ == '__main__':
