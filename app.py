7d31876d154655c86ce09df44ef1c99ece693196
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-22 12:31:41 -0400

    stock prices interactive chart

--- app.py
+++ app.py
@@ -1,93 +1,106 @@
-#######
-# This shows the mpg.csv dataset as a spread out scatter plot
-# that sends hoverData to another graph via callback, and to
-# a Markdown component through a second callback.
-######
-import os
 import datetime
 import dash
 import dash_core_components as core
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.graph_objs as go
-import pandas as pd
+from dash.dependencies import Input, Output, State
+import dash.exceptions as dash_exceptions
 import pandas_datareader as data
-from numpy import random
 
-try:
-    df = pd.read_csv('data/stocks.csv', index_col=None)
-except FileNotFoundError:
-    df = None
 
-if df is None:
-    print('Retrieving data from yahoo api...')
-    tickers = ['WORK', 'HA', 'NGVC']
-    start_date = pd.to_datetime('2020-01-01')
-    end_date = pd.to_datetime('today')
-
-    df = []
+def get_figure(tickers, start_date, end_date, title="Closing Stock Prices"):
+    print(tickers)
+    traces = []
     for ticker in tickers:
-        temp = data.DataReader(ticker, 'yahoo', start_date, end_date)
-        if temp is not None:
-            temp.insert(0, 'Ticker', ticker)
-            temp.reset_index(level=0, inplace=True)
-            df.append(temp)
-    if df:
-        df = pd.concat(df, ignore_index=True)
-        df.to_csv('data/stocks.csv', index=False)
+        df = get_data(ticker, start_date, end_date)
+        traces.append({"x": df.index, "y": df["Close"], "name": ticker})
+    fig = {
+        "data": traces,
+        "layout": {
+            "title": title
+        }
+    }
+    return fig
 
+
+def get_data(ticker, start_date, end_date):
+    df = data.DataReader(ticker, 'yahoo', start_date, end_date)
+    if df is not None:
+        df.insert(0, 'Ticker', ticker)
+        df.reset_index(level=0, inplace=True)
+    return df
+
+
+options = [
+    {"label": "WORK", "value": "WORK"},
+    {"label": "HA", "value": "HA"},
+    {"label": "NGVC", "value": "NGVC"}
+]
+
+print(options[0]["value"])
+print([options[0]["value"]])
 app = dash.Dash()
 
 app.layout = html.Div(
     className="container",
     children=[
+
         html.Div(
             className="4d7000",
             children=[
+
                 html.Div(
                     className="2e6000",
                     children=[
                         html.Div(
                             className="2e6003",
-                            id="ddl_stocks",
                             children=[
                                 "Select Stock Ticker: ",
-                                core.Dropdown()
+                                core.Dropdown(
+                                    id="ddl_stocks",
+                                    options=options,
+                                    multi=True,
+                                    value=[options[0]["value"]]
+                                )
                             ]
                         ),
+
                         html.Div(
                             className="2e6006",
-                            id="dtp_dates",
                             children=[
                                 "Select Start and End Dates: ",
-                                core.DatePickerRange()
+                                core.DatePickerRange(
+                                    id="dtp_dates",
+                                    min_date_allowed=datetime.date(2015, 1, 1),
+                                    max_date_allowed=datetime.date.today(),
+                                    start_date=datetime.date(2020, 1, 1),
+                                    end_date=datetime.date.today()
+                                )
                             ]
                         ),
                         html.Div(
                             className="2e6009",
-                            id="btnSubmit",
                             children=[
-                                html.Button("Submit")
+                                html.Button(
+                                    id="btn_submit",
+                                    children=[
+                                        "Submit"
+                                    ]
+                                )
                             ]
                         )
                     ]
                 ),
+
                 html.Div(
                     className="42c090",
                     children=[
                         core.Graph(
                             id="stocks_graph",
-                            figure={
-                                "data": [
-                                    {
-                                        "x": [1, 2, 3],
-                                        "y": [5, 7, 6]
-                                    }
-                                ],
-                                "layout": {
-                                    "title": "Stocks Graph"
-                                }
-                            }
+                            figure=get_figure(
+                                [options[0]["value"]],
+                                '2020-01-01',
+                                datetime.date.today().isoformat()
+                            )
                         )
                     ]
                 )
@@ -98,24 +111,31 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output("stocks_graph", "figure"),
-    [Input("ddl_stocks", "value")]
+    Output("ddl_stocks", "options"),
+    [Input("ddl_stocks", "search_value")],
 )
-def update_title(arg):
-    if arg is None:
-        arg = "Foo"
-    figure = {
-        "data": [
-            {
-                "x": [1, 2, 3],
-                "y": [5, 7, 8]
-            }
-        ],
-        "layout": {
-            "title": arg
-        }
-    }
-    return figure
+def update_options(search_value):
+    if not search_value:
+        raise dash_exceptions.PreventUpdate
+    return [opt for opt in options if search_value in opt["label"]]
+
+
+@app.callback(
+    Output("stocks_graph", "figure"),
+    [Input("btn_submit", "n_clicks")],
+    [
+        State("ddl_stocks", "value"),
+        State("dtp_dates", "start_date"),
+        State("dtp_dates", "end_date")
+    ]
+)
+def update_graph(n_clicks, tickers, start_date, end_date):
+    fig = get_figure(
+        tickers=tickers,
+        start_date=start_date,
+        end_date=end_date
+    )
+    return fig
 
 
 if __name__ == '__main__':
